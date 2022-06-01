3e9a92a8d4551bffb41655c996e0434fe63e13ff
a2tikhonov <git!St@d1um@rc@d1um>
2022-06-01 17:07:04 +0300

    Сделал домашку

---
+++ src/exceptions/WrongPasswordException.java
@@ -0,0 +1,20 @@
+package exceptions;
+
+import java.io.IOException;
+
+public class WrongPasswordException extends IOException {
+    public WrongPasswordException() {
+    }
+
+    public WrongPasswordException(String message) {
+        super(message);
+    }
+
+    public WrongPasswordException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public WrongPasswordException(Throwable cause) {
+        super(cause);
+    }
+}
